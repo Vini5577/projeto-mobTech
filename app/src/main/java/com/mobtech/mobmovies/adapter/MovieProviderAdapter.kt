967aab8716c22812ca8b83cc0ac26645ffcf97d3
caiomnsilva <caiomnasilva@gmail.com>
2024-06-13 15:22:50 -0300

    fix null provider

--- app/src/main/java/com/mobtech/mobmovies/adapter/MovieProviderAdapter.kt
+++ app/src/main/java/com/mobtech/mobmovies/adapter/MovieProviderAdapter.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.view.isGone
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -19,30 +20,42 @@ import com.mobtech.mobmovies.R
 import com.mobtech.mobmovies.data.FlatRateData
 import com.mobtech.mobmovies.data.ProviderData
 
-class MovieProviderAdapter(private val movieProvider: List<FlatRateData>, private val context: Context) {
+class MovieProviderAdapter(private val movieProvider: List<FlatRateData>?, private val context: Context) {
 
     fun bindView(linearLayout: LinearLayout) {
         val inflater = LayoutInflater.from(context)
+        val providers = movieProvider ?: emptyList()
 
-        for (provider in movieProvider) {
+
+        if (providers.isEmpty()) {
             val view = inflater.inflate(R.layout.provider_card, linearLayout, false)
-            val imageView: ImageView = view.findViewById(R.id.img_provider)
             val textView: TextView = view.findViewById(R.id.txt_provider)
-
-            if(provider.logo_path != null) {
-                Glide.with(context)
-                    .load("https://image.tmdb.org/t/p/w185${provider.logo_path}")
-                    .into(imageView)
-
-                imageView.visibility = View.VISIBLE
-                textView.visibility = View.GONE
-            } else {
-                textView.text = "Não disponível em nenhum streaming"
-                imageView.visibility = View.GONE
-                textView.visibility = View.VISIBLE
-            }
-
+            val cardView: CardView = view.findViewById(R.id.cardProvider)
+            cardView.visibility = View.GONE
+            textView.text = "Não está disponível em nenhum streaming."
             linearLayout.addView(view)
+        } else {
+            for (provider in providers) {
+                val view = inflater.inflate(R.layout.provider_card, linearLayout, false)
+                val imageView: ImageView = view.findViewById(R.id.img_provider)
+                val textView: TextView = view.findViewById(R.id.txt_provider)
+
+                if(provider.logo_path != null) {
+                    Glide.with(context)
+                        .load("https://image.tmdb.org/t/p/w185${provider.logo_path}")
+                        .into(imageView)
+
+                    imageView.visibility = View.VISIBLE
+                    textView.visibility = View.GONE
+                } else {
+                    textView.text = "Não disponível em nenhum streaming"
+                    imageView.visibility = View.GONE
+                    textView.visibility = View.VISIBLE
+                }
+
+                linearLayout.addView(view)
+            }
         }
     }
 }
+
--- app/src/main/java/com/mobtech/mobmovies/adapter/SerieProviderAdapter.kt
+++ app/src/main/java/com/mobtech/mobmovies/adapter/SerieProviderAdapter.kt
@@ -6,34 +6,45 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.bumptech.glide.Glide
 import com.mobtech.mobmovies.R
 import com.mobtech.mobmovies.data.SerieFlatRateData
 
-class SerieProviderAdapter(private val serieProvider: List<SerieFlatRateData>, private val context: Context) {
+class SerieProviderAdapter(private val serieProvider: List<SerieFlatRateData>?, private val context: Context) {
 
     fun bindView(linearLayout: LinearLayout) {
         val inflater = LayoutInflater.from(context)
+        val providers = serieProvider ?: emptyList()
 
-        for (provider in serieProvider) {
+        if (providers.isEmpty()) {
             val view = inflater.inflate(R.layout.provider_card, linearLayout, false)
-            val imageView: ImageView = view.findViewById(R.id.img_provider)
             val textView: TextView = view.findViewById(R.id.txt_provider)
-
-            if(provider.logo_path != null) {
-                Glide.with(context)
-                    .load("https://image.tmdb.org/t/p/w185${provider.logo_path}")
-                    .into(imageView)
-
-                imageView.visibility = View.VISIBLE
-                textView.visibility = View.GONE
-            } else {
-                textView.text = "Não disponível em nenhum streaming"
-                imageView.visibility = View.GONE
-                textView.visibility = View.VISIBLE
-            }
-
+            val cardView: CardView = view.findViewById(R.id.cardProvider)
+            cardView.visibility = View.GONE
+            textView.text = "Não está disponível em nenhum streaming"
             linearLayout.addView(view)
+        } else {
+            for (provider in providers) {
+                val view = inflater.inflate(R.layout.provider_card, linearLayout, false)
+                val imageView: ImageView = view.findViewById(R.id.img_provider)
+                val textView: TextView = view.findViewById(R.id.txt_provider)
+
+                if(provider.logo_path != null) {
+                    Glide.with(context)
+                        .load("https://image.tmdb.org/t/p/w185${provider.logo_path}")
+                        .into(imageView)
+
+                    imageView.visibility = View.VISIBLE
+                    textView.visibility = View.GONE
+                } else {
+                    textView.text = "Não disponível em nenhum streaming"
+                    imageView.visibility = View.GONE
+                    textView.visibility = View.VISIBLE
+                }
+
+                linearLayout.addView(view)
+            }
         }
     }
 }
